b6c58a4f51f38dacaba7d0a31d075b9a0d231f88
Joseph C <jciv.coding@gmail.com>
2018-10-23 11:17:08 -0400

    DB set-up, No errors, Tables Created Successfully

--- server/server.js
+++ server/server.js
@@ -3,14 +3,11 @@ const app = express();  // ?? This is basically us starting up the server
 const port = 3000;
 const path = require('path');
 
-<<<<<<< HEAD
 const db = require('./database/elephantsql.js')
-=======
-app.use(express.static('./dist'))
->>>>>>> bec0213de3a9ee4fdbb2b958978efd169b7a2f65
 
 db.sequelize.sync().then(function() {
   // MAKING SURE DATABASE TABLES & MODELS GET ASSOCIATED BEFORE STARTING UP THE SERVER
+  app.use(express.static('./dist'))
   app.use(express.static('./static'))
 
   app.get('/', (req,res) => {
