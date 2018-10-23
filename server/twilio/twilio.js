e433a393b6930ac27c31264eb9687681f6b8edc9
Mike <mmarceca@aardvarkbrigade.com>
2018-10-23 11:55:19 -0400

    added twilio to dependencys and added basic script for twilio call

---
+++ server/twilio/twilio.js
@@ -0,0 +1,12 @@
+var accountSid = 'AC6449e7510867d3a5c658ea2b39f24227'; // Your Account SID from www.twilio.com/console
+var authToken = '451bd935048fede9a44b505c60e25e21';   // Your Auth Token from www.twilio.com/console
+
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'Your appointment is in ' + 'days go here',
+    to: '+',  // Text this number
+    from: '+19564773577' // From a valid Twilio number
+})
+.then((message) => console.log('then after twilio ',message.sid));
