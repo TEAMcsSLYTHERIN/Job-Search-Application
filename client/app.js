7bfdc359a0ed1b58f41448f5cd82512d99b050f6
christinechoi <smi77y@gmail.com>
2018-10-24 13:51:43 -0400

    Add google signin and corresponding route

--- client/app.js
+++ client/app.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import { debug } from 'util';
+import { GoogleLogin } from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 
 class App extends Component {
+    
+  responseGoogle(response) {
+    fetch('/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `${response.tokenId}`
+      }})
+    .then(res => res.json())
+    .then(res => {
+      console.log('res', res);
+    })
+  }
+
+  handleFail(e) {
+    console.log('in failure', e);
+  }
+
+  signOut() {
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(res => {
+      console.log('User signed out.', res);
+    });
+  }
+
   render(){
     return (
-      <div className="container">Hello World</div>
+      <div> 
+        <GoogleLogin
+          onClick={() => {this.onSignIn}}
+          clientId='785379560416-oee86k0flmbp00qkc52mcvaoqs6g307a.apps.googleusercontent.com'
+          buttonText='Login'
+          onSuccess={this.responseGoogle}
+          onFailure={this.handleFail}
+        />
+
+        <GoogleLogout
+          buttonText='Logout'
+          onLogoutSuccess={this.signOut}
+        />
+        <div className='container'>Hello World</div>
+      </div>
     );
   }
+  
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const fetch = require('node-fetch');
 
 app.use(express.static('./dist'))
 
 app.get('/', (req,res) => {
   res.sendFile('index.html')
 })
+
+app.post('/auth', (req, res, next) => {
+  next();
+  },
+  (req, res) => {
+    fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.headers.authorization}`)
+    .then(res => {
+      console.log('RES STATUS', res.status)
+      let status = res.status 
+
+      // if (status === 200) // check db for user 
+        // if user exists in db, create authenticated session with timeout 
+      
+        // if not, create new user from id token payload and create new session 
+      
+        // You can prompt the user for any additional profile information you require when you detect a newly created user in your app.
+    })
+  },
+)
 
 app.listen(port, () => {
   console.log('listening on port: ' + port)
