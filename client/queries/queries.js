469dd2a4a69d3a82dc6db27830ab1c4bacd9af2f
Mike <mmarceca@aardvarkbrigade.com>
2018-10-25 12:08:03 -0400

    added queries folder and started with a hard coded query for a use and infrmation for testing

---
+++ client/queries/queries.js
@@ -0,0 +1,35 @@
+allUserData = `
+{
+  user(UserName: "Draco") {
+    id
+    firstName
+    lastName
+    password
+    email
+    phone
+    applications {
+    	companyName
+      title
+      dateApplied
+      link
+      description
+      notes
+      notification
+      contactId
+      UserId
+      contact {
+        firstName
+        lastName
+        email
+        phone
+      }
+    }
+  }
+}
+`
+
+
+
+module.exports = {
+  allUserData
+}
